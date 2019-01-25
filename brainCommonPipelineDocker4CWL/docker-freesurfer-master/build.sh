d07358eda5fd935404e0f4ec45b8209cf6290eae
Xingyan Kuang <kuangx@uchicago.edu>
2019-01-25 13:01:13 -0600

    deleted duplicated brainCommonPipelineDocker4CWL
    Signed-off-by: Xingyan Kuang <kuangx@uchicago.edu>

--- brainCommonPipelineDocker4CWL/docker-freesurfer-master/build.sh
+++
@@ -1,3 +0,0 @@
-docker build -t brainlife/freesurfer . && \
-    docker tag brainlife/freesurfer brainlife/freesurfer:6.0.0 && \
-    docker push brainlife/freesurfer
--- brainCommonPipelineDocker4CWL/docker-freesurfer-master/initialize_subDir.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-dir_name=$1
-
-cp -r /usr/local/freesurfer/subjects/fsaverage  $dir_name
